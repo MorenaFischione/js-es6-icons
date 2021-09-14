51b522bcb6e52f98ba794b605e52a05887fdfa67
MorenaFischione <fischione.morena@gmail.com>
2021-09-14 09:31:35 +0200

    2 Milestone da completare

--- js/my_script.js
+++ js/my_script.js
@@ -116,26 +116,64 @@ const icons =
 
 let container = document.getElementById("icon");
 
-printToPage(icons, container);
 
+// Milestone 2
+// Coloriamo le icone per tipo
 
+const colors = ["magenta", "lime", "turquoise"];
+
+colorizedIcons = colorizeItems(icons, colors);
+
+printToPage(colorizedIcons, container);
 
 // ****** FUNCTION *******
 
 // FUNZIONE che mostra in pagina tutti gli elementi dell'Array Icons.
-
-
 function printToPage(array, container){
     let htmlContent = "";
     array.forEach(element => {
-        const {name, prefix, type, family} = element;
+        const {name, prefix, type, family, color} = element;
 
         htmlContent += 
         `<div>
-            <i class="${family} ${prefix}${name}"></i>
+            <i class="${family} ${prefix}${name}
+            style="color: ${color}"></i>
             <h4 class="title">${name}</h4>
          </div>
         `;
     });
     container.innerHTML = htmlContent;
 };
+
+
+// FUNZIONE 
+// che prende: 
+// - un array di oggetti  
+// - il nome della proprietà da analizzare 
+// e ritorna un nuovo array di valori univoci di quella proprietà nell'array data.
+function getUniquePropertyValues(array, property){
+    const types = [];
+
+    array.forEach((element, index) => {
+        if (! types.includes(element[property])){
+            types.push(element[property]);
+        };
+    });
+
+    return types;
+}
+
+
+function colorizeItems(array, colors){
+    const types = getUniquePropertyValues(array, "type");
+    console.log(types);  
+
+    const colorizedArray = array.map((element) => {
+        const indexOfTipe = types.indexOf(element.type);
+        if(indexOfTipe !== -1 ){
+            element.color = colors[indexOfTipe];
+        }
+        return element;
+    });
+    return colorizedArray;
+}
